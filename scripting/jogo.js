ccc1dbc695085d9c447ce4abb5eef44baa6f1c5b
José David <50461429+DaveJosef@users.noreply.github.com>
2019-12-30 12:27:50 -0300

    Add files via upload

---
+++ scripting/jogo.js
@@ -0,0 +1,20 @@
+//////////////////////////////////////////////////*
+//
+//  Jogo da Palavra Misteriosa
+//  
+///////////////////////////////////////////////////
+
+
+var palavra = "";
+var tamanho = 5;
+
+function juntarLetra( letra, palavra, tamanho ){
+    if( palavra.length < tamanho ){
+        palavra = palavra + letra;
+        return 1;
+    }
+}
+
+function pegarLetra( elemento ){
+    return elemento.value;
+}
